8bd196978e819b4ec4476d274cb7c5cdd22c8d66
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2026-01-18 17:15:18 +0100

    Create 02_webexbot_domain.py

---
+++ 02-interactive/02_webexbot_domain.py
@@ -0,0 +1,22 @@
+"""
+Cisco Live 2026 - LABCOL-1007: Building Your First Webex Bot
+
+- Diego Manuel Jimenez Moreno
+"""
+
+import os
+
+# Load environment variables from the .env file.
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+domain = os.getenv("DOMAIN")
+
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.webex_bot import WebexBot
+
+# Create a Bot Object
+bot = WebexBot(teams_bot_token=BOT_TOKEN,
+               bot_name="CiscoLive2026",
+               approved_domains=DOMAIN,
+               include_demo_commands=True)
