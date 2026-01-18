d3b059a763e31e4615a5a1ec4476b835c74f90e4
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2026-01-18 17:29:01 +0100

    Create 03_webexbot_function.py

---
+++ 02-interactive/03_webexbot_function.py
@@ -0,0 +1,69 @@
+"""
+Cisco Live 2026 - LABCOL-1007: Building Your First Webex Bot
+
+- Diego Manuel Jimenez Moreno
+"""
+
+import os
+
+# Load environment variables from the .env file.
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+domain = os.getenv("DOMAIN")
+
+from webex_bot.formatting import quote_info
+from webex_bot.models.command import Command
+from webex_bot.models.response import Response
+
+class AutoProvisioning(Command):
+
+    def __init__(self):
+        super().__init__(
+            command_keyword="provision",
+            help_message="Provision your new IP Phone",
+            chained_commands=[ProvisionCallback()])
+
+    def execute(self, message, attachment_actions, activity):
+        """
+        :param message: message with command already stripped
+        :param attachment_actions: attachment_actions object
+        :param activity: activity object
+
+        :return: a string or Response object (or a list of either). Use Response if you want to return another card.
+        """
+        card = {
+            ...
+        }
+
+        response = Response()
+        response.text = "Text is needed when sending a card."
+        response.attachments = card
+
+        return response
+
+class ProvisionCallback(Command):
+
+    def __init__(self):
+        super().__init__(
+            card_callback_keyword="provision_callback",
+            delete_previous_message=False)
+
+    def execute(self, message, attachment_actions, activity):
+        return quote_info(attachment_actions.inputs.get("mac_address"))
+
+
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.webex_bot import WebexBot
+
+# Create a Bot Object
+bot = WebexBot(teams_bot_token=BOT_TOKEN,
+               bot_name="CiscoLive2026,
+               approved_domains=DOMAIN,
+               include_demo_commands=True)
+
+# Add new commands for the bot to listen out for.
+bot.add_command(AutoProvisioning())
+
+# Call `run` for the bot to wait for incoming messages.
+bot.run()
