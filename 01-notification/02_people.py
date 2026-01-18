7b4b381eb97f9fb6cc7d502b7ea32671ad3f9e8a
Diego M Jimenez <diego.jimenez.2007@gmail.com>
2026-01-18 13:51:42 +0100

    Create 02_people.py

---
+++ 01-notification/02_people.py
@@ -0,0 +1,23 @@
+"""
+Cisco Live 2026 - LABCOL-1007: Building Your First Webex Bot
+
+- Diego Manuel Jimenez Moreno
+"""
+
+import os
+from dotenv import load_dotenv
+from webexpythonsdk import WebexAPI
+
+# Load environment variables from the .env file.
+load_dotenv()
+
+access_token = os.getenv("ACCESS_TOKEN")
+
+webex = WebexAPI(access_token=ACCESS_TOKEN)
+
+try:
+  all_people = webex.people.list()
+  for person in all_people:
+      print(f"Name: {person.displayName}, Email: {person.emails}")
+except Exception as e:
+  print(f"An error occurred: {e}")
